134308000f85a0a1bf0cea5af8f0bdf481dd6e5f
Sabiha Bary <sabihaab9@gmail.com>
2022-06-06 14:41:28 -0400

    commit tree 670e3b03840617f162638c5c21ee12ef8f1d2d0b

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,21 @@
-import React from "react";
+// import React from "react";
 
-function Article() {
-  return (
-    <div>
-      {/* please pass this test */}
-    </div>
-  );
-}
+// function Article() {
+//   return (
+//     <div>
+//       {"please pass this test"}
+//     </div>
+//   );
+// }
 
-export default Article;
+// export default Article;
+
+// src/__tests__/Article.test.js
+test("displays the text 'please pass this test'", () => {
+  render(<Article />);
+
+  // add this line
+  screen.debug();
+
+  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+});
